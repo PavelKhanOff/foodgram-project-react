448f49d8d82f581067e929d6191409a978ee155b
PavelKhan <pavelkhanoff@gmail.com>
2021-07-20 13:25:43 +0600

    Добавлен подсчет подписчиков в админку

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -5,6 +5,11 @@ from .models import Favorites, Follow, Ingredient, Recipe, ShoppingList, Tag
 
 class RecipeAdmin(admin.ModelAdmin):
     list_filter = ('author', 'name', 'tags')
+    list_display = ('name', 'followers')
+
+    @admin.display(empty_value=None)
+    def followers(self, obj):
+        return len(obj.favorite_recipe.all())
 
 
 class IngredientAdmin(admin.ModelAdmin):
